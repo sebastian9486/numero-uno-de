3ededd2f1fd0c9c5ae4a785b7c0ae6e133bcf119
sebastian <sommerfeld.sebastian@gmail.com>
2021-02-03 11:43:49 +0100

    added webserver script

---
+++ run-local.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+LOG_DONE="[\e[32mDONE\e[0m]"
+LOG_ERROR="[\e[1;31mERROR\e[0m]"
+LOG_INFO="[\e[34mINFO\e[0m]"
+LOG_WARN="[\e[93mWARN\e[0m]"
+
+echo -e "$LOG_INFO starting webserver for construction-site"
+webserver 9001 src/main/construction-site
